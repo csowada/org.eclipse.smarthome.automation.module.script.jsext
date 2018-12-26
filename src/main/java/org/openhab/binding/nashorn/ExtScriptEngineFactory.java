e1bdc1ec6e7cc1e3038738cc99c92f8d140107fb
Christian S. <git@cs-dev.de>
2018-12-26 12:08:04 +0100

    update

--- src/main/java/org/openhab/binding/nashorn/ExtScriptEngineFactory.java
+++ src/main/java/org/openhab/binding/nashorn/ExtScriptEngineFactory.java
@@ -78,6 +78,11 @@ public class ExtScriptEngineFactory implements ScriptEngineFactory {
 
             try {
                 Require.enable((NashornScriptEngine) engine, rootFolder);
+
+                // engine.eval("var global = this;\n" + "var window = this;\n" + "var process = {env: {}};\n" + "\n"
+                // + "var console = {};\n" + "console.debug = print;\n" + "console.warn = print;\n"
+                // + "console.loggy = print;");
+
             } catch (ScriptException e) {
                 logger.error("error!", e);
             }

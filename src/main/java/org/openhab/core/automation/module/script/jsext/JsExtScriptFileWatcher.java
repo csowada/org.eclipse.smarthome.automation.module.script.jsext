f647a86808c81afe674278041cb93354ad4353c9
Christian S. <git@cs-dev.de>
2020-04-01 18:19:59 +0200

    Rename package to org.openhab.core.automation.module.script.jsext

--- src/main/java/org/openhab/core/automation/module/script/jsext/JsExtScriptFileWatcher.java
+++ src/main/java/org/openhab/core/automation/module/script/jsext/JsExtScriptFileWatcher.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.smarthome.automation.module.script.jsext;
+package org.openhab.core.automation.module.script.jsext;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
